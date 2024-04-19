22e1d9fc4cb4a87396546c0a49bc74b0ba80b4f6
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-19 17:32:12 +0200

    added div inside listNumber.innerHTML

--- js/main.js
+++ js/main.js
@@ -7,24 +7,23 @@ for (numberVariable = 1; numberVariable <=100; numberVariable++) {
     //Creo div in pagina e gli do il valore dei numeri da 1 a 100
     let listNumber = document.querySelector("div.number")
         const div = `<div>${numberVariable}</div>`;
-        
     //Multipli di 3e5
     if (numberVariable % 3 == 0 && numberVariable % 5 == 0) {
         console.log(numberVariable,"FizzBuzz")
         //immetto in Pagina fizzbuzz ai multipli di 3e5
-        listNumber.innerHTML += "FizzBuzz"
+        listNumber.innerHTML += `<div>FizzBuzz</div>`
     }
     //Multipli di 3  
         else if (numberVariable % 3 ==0) {
         console.log(numberVariable,"Fizz")
         //immetto in Pagina fizzbuzz ai multipli di 3
-        listNumber.innerHTML += "Fizz"
+        listNumber.innerHTML += `<div>Fizz</div>`
     } 
     //Multipli di 5
         else if (numberVariable % 5 ==0) {
         console.log(numberVariable,"Buzz")
         //immetto in Pagina fizzbuzz ai multipli di 5
-        listNumber.innerHTML += "Buzz"
+        listNumber.innerHTML += `<div>Buzz</div>`
     }
     //TuTTi gli altri Numeri  
         else {

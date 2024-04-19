73a5a45f31f9d70be2fc7af2e5a0b5b18b346366
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-19 17:05:26 +0200

    create div inside html with js

--- js/main.js
+++ js/main.js
@@ -4,15 +4,25 @@ let numberVariable = 1
 //Creazione ciclo FOR
 for (numberVariable = 1; numberVariable <=100; numberVariable++) {
 
+    //Multipli di 3e5
     if (numberVariable % 3 == 0 && numberVariable % 5 == 0) {
         console.log(numberVariable,"FizzBuzz")
-    } else if (numberVariable % 3 ==0) {
+    }
+    //Multipli di 3  
+        else if (numberVariable % 3 ==0) {
         console.log(numberVariable,"Fizz")
-    } else if (numberVariable % 5 ==0) {
+    } 
+    //Multipli di 5
+        else if (numberVariable % 5 ==0) {
         console.log(numberVariable,"Buzz")
-    } else {
+    }
+    //TuTTi gli altri Numeri  
+        else {
         console.log(numberVariable)
-    }    
-}
-
-//Ogni multiplo di 3 si chiama fizz
+        
+    }
+    //Creo div in pagina e gli do il valore dei numeri da 1 a 100
+    let listNumber = document.querySelector("div.number")
+        const div = `<div>${numberVariable}</div>`;
+        listNumber.innerHTML += div
+}        

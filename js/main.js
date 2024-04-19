727dd9745bb49fee57a713352a1b4795386aab6b
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-19 16:02:20 +0200

    added multiple 3,5 and 3&5

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 console.log("ciao Giuseppe")
 
+let numberVariable = 1
 //Creazione ciclo FOR
-for (let numberVariable = 1; numberVariable <=100; numberVariable++) {
-    console.log("Numero:", numberVariable)
+for (numberVariable = 1; numberVariable <=100; numberVariable++) {
+
+    if (numberVariable % 3 == 0 && numberVariable % 5 == 0) {
+        console.log(numberVariable,"FizzBuzz")
+    } else if (numberVariable % 3 ==0) {
+        console.log(numberVariable,"Fizz")
+    } else if (numberVariable % 5 ==0) {
+        console.log(numberVariable,"Buzz")
+    } else {
+        console.log(numberVariable)
+    }    
 }
+
+//Ogni multiplo di 3 si chiama fizz
